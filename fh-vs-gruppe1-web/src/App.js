9ef83407a8e52b393c453362c63e447598c4d116
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2022-12-05 20:29:54 +0100

    *initial commit

--- fh-vs-gruppe1-web/src/App.js
+++ fh-vs-gruppe1-web/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test com
+          Learn React test comfds
         </a>
       </header>
     </div>

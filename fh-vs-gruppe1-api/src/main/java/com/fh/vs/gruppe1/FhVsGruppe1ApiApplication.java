cb0396bccad09210190f0d02d7895e4b38b0c6a7
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2022-12-05 20:21:10 +0100

    Initial commit

---
+++ fh-vs-gruppe1-api/src/main/java/com/fh/vs/gruppe1/FhVsGruppe1ApiApplication.java
@@ -0,0 +1,21 @@
+package com.fh.vs.gruppe1;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhVsGruppe1ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhVsGruppe1ApiApplication.class, args);
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        logger.setLevel(Level.ALL);
+
+        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>Ready to go<<<<<<<<<<<<<<<<<<<<<<<<<");
+
+    }
+
+}
---
+++ fh-vs-gruppe1-api/src/test/java/com/fh/vs/gruppe1/FhVsGruppe1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh.vs.gruppe1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhVsGruppe1ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

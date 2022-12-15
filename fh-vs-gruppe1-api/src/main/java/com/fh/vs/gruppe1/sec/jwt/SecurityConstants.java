51d25c12d896cc44d5632f00670cfff7fc04681d
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2022-12-15 16:15:54 +0100

    *increased token exp date

--- fh-vs-gruppe1-api/src/main/java/com/fh/vs/gruppe1/sec/jwt/SecurityConstants.java
+++ fh-vs-gruppe1-api/src/main/java/com/fh/vs/gruppe1/sec/jwt/SecurityConstants.java
@@ -1,6 +1,6 @@
 package com.fh.vs.gruppe1.sec.jwt;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 999999999;
     public static final String JWT_SECRET = "geheim";
 }

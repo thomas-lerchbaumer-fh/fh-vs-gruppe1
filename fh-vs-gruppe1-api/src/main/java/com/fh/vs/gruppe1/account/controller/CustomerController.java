4b58d7417edf190dd77f97fdd3be668613ddab3b
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2023-01-13 15:08:58 +0100

    *added getCustomer (on customer login) + adapted buyStock logic

--- fh-vs-gruppe1-api/src/main/java/com/fh/vs/gruppe1/account/controller/CustomerController.java
+++ fh-vs-gruppe1-api/src/main/java/com/fh/vs/gruppe1/account/controller/CustomerController.java
@@ -4,6 +4,8 @@ package com.fh.vs.gruppe1.account.controller;
 import com.fh.vs.gruppe1.account.Customer;
 import com.fh.vs.gruppe1.account.repository.CustomerRepository;
 import com.fh.vs.gruppe1.bank.service.BankService;
+import com.fh.vs.gruppe1.depot.Depot;
+import com.fh.vs.gruppe1.transaction.ClientOrder;
 import com.fh.vs.gruppe1.transaction.ClientOrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,12 +17,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -40,32 +40,54 @@ public class CustomerController {
 
     @Autowired
     public BankService bankService;
+
     @RequestMapping("/loadUser")
-    public String person(){
+    public String person() {
         return "Hello World";
     }
 
     @PostMapping("/buyStock")
-    public ResponseEntity<Customer> buyStock(Authentication authentication,@RequestBody Map<String,String> request){
+    public ResponseEntity<ClientOrder> buyStock(Authentication authentication, @RequestBody Map<String, String> request) {
         String symbol = request.get("symbol");
         Integer amount = Integer.valueOf(request.get("amount"));
         Optional<Customer> customer = customerRepository.findByEmail(authentication.getName());
-    log.info(authentication.getName() + "customer request");
-        if(customer.isEmpty()){
+
+        log.info(authentication.getName() + "customer request");
+
+        if (customer.isEmpty()) {
             log.warn("customer cannot be found via active sess toke, access denied");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "your message string");
         }
-
         String userEmail = customer.get().getEmail();
-
         log.info(userEmail + " my user email @customer login");
 
-        Customer response = bankService.buyStock(symbol,amount,userEmail);
+        ClientOrder response = bankService.buyStock(symbol, amount, userEmail);
         return new ResponseEntity<>(response, HttpStatus.OK);
-
     }
 
+    @GetMapping("/getCustomer")
+    public ResponseEntity<Customer> getDepot(Authentication authentication){
+        Optional<Customer> customerSearch = customerRepository.findByEmail(authentication.getName());
+        log.info(authentication.getName() + "customer request");
+        if (customerSearch.isEmpty()) {
+            log.warn("customer cannot be found via active sess toke, access denied");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "customer cannot be found via active sess toke, access denied");
 
+        }
+        Customer customer = customerSearch.get();
+        List<ClientOrder> co = customer.getDepot().getTransactions();
+        if(co != null){
+            co.forEach(order ->{
+                double currentSharePrice = bankService.getCurrentShareValue(order.getSymbol());
+                order.setCurrentPrice(currentSharePrice);
+            });
+            customer.getDepot().setCurrentTotalDepotValue(
+                    customer.getDepot().getTransactions().stream().mapToDouble(item -> item.getCurrentPrice() * item.getAmount()).sum()
+            );
+        }
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+
+    }
 
 
 }

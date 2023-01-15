fc39d3581461315ff5f4d4863a47041f285c1856
hmvir <harmanvir.singh@stud.fh-campuswien.ac.at>
2023-01-15 11:13:16 +0100

    Added Autowiring Added Object Relation (Person & Address) for Persistence

--- fh-vs-gruppe1-api/src/main/java/com/fh/vs/gruppe1/account/service/EmployeeService.java
+++ fh-vs-gruppe1-api/src/main/java/com/fh/vs/gruppe1/account/service/EmployeeService.java
@@ -6,14 +6,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
 
+    @Autowired
     private final EmployeeRepository repo;
 
     public Employee saveEmployee(Employee employee) {
         return repo.save(employee);
+    }
+
+    public Employee findEmployee(Employee employee){
+        return repo.findByEmail(employee.getEmail()).get();
     }
 
     public void deleteEmployee(String id) {
